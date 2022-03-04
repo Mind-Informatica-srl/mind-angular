526dc8341529e99d7194b108412b0635d4d02729
lorenzoB07 <82948587+lorenzoB07@users.noreply.github.com>
2022-03-04 15:28:05 +0100

    Update mind-angular-lib.module.ts

--- projects/mind-angular-lib/src/lib/mind-angular-lib.module.ts
+++ projects/mind-angular-lib/src/lib/mind-angular-lib.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [ConfirmDialogComponent],
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, CommonModule],
   exports: [ConfirmDialogComponent],
 })
 export class MindAngularLibModule {}
